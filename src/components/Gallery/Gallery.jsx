36c82c55c409e28276d771a580b972ccf0aba561
p.chudnoff <p.chudnoff@yandex.ru>
2023-04-08 15:10:01 +0300

    gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,10 +1,38 @@
 import './styles.css'
+import {configGallery} from "../../config-gallery";
+import {useState} from "react";
+import {Modal, Box} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 const Gallery = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [image, setImage] = useState('')
+  const navigate = useNavigate()
+
+  const handleSelect = (i) => {
+    setImage(i)
+    setIsOpen(true)
+  }
+
+  const handleClose = () => {
+    setIsOpen(false)
+    setImage('')
+
+  }
 
   return (
     <div className={'gallery-container'}>
-          <img src={`/gallery/allphoto.jpg`} alt={'allphoto'}/>
+      {configGallery.map((i) => <img onClick={()=>handleSelect(i.img)} className={'gallery-card'} src={`/gallery/${i.img}`} alt={`${i.key}`}/>)}
+      <div onClick={()=> navigate('/contacts')} className={'buy-button'}>Купить работы</div>
+      <Modal
+        open={isOpen}
+        aria-describedby="modal-modal-description"
+      >
+          <Box id="modal-modal-description">
+            <img className={'modal-card'} src={`/gallery/${image}`} alt={`${image}`}/>
+            <div className={'close-button'} onClick={handleClose}/>
+          </Box>
+      </Modal>
     </div>  )
 }
 
---
+++ src/config-gallery.js
@@ -0,0 +1,251 @@
+export const configGallery = [
+  {
+    key: 1,
+    img: 'gallery-1.jpg'
+  },
+  {
+    key: 2,
+    img: 'gallery-2.jpg'
+  },
+  {
+    key: 3,
+    img: 'gallery-3.jpg'
+  },
+  {
+    key: 4,
+    img: 'gallery-4.jpg'
+  },
+  {
+    key: 5,
+    img: 'gallery-5.jpg'
+  },
+  {
+    key: 6,
+    img: 'gallery-6.jpg'
+  },
+  {
+    key: 7,
+    img: 'gallery-7.jpg'
+  },
+  {
+    key: 8,
+    img: 'gallery-8.jpg'
+  },
+  {
+    key: 9,
+    img: 'gallery-9.jpg'
+  },
+  {
+    key: 10,
+    img: 'gallery-10.jpg'
+  },
+  {
+    key: 11,
+    img: 'gallery-11.jpg'
+  },
+  {
+    key: 12,
+    img: 'gallery-12.jpg'
+  },
+  {
+    key: 13,
+    img: 'gallery-13.jpg'
+  },
+  {
+    key: 14,
+    img: 'gallery-14.jpg'
+  },
+  {
+    key: 15,
+    img: 'gallery-15.jpg'
+  },
+  {
+    key: 16,
+    img: 'gallery-16.jpg'
+  },
+  {
+    key: 17,
+    img: 'gallery-17.jpg'
+  },
+  {
+    key: 18,
+    img: 'gallery-18.jpg'
+  },
+  {
+    key: 19,
+    img: 'gallery-19.jpg'
+  },
+  {
+    key: 20,
+    img: 'gallery-20.jpg'
+  },
+  {
+    key: 21,
+    img: 'gallery-21.jpg'
+  },
+  {
+    key: 22,
+    img: 'gallery-22.jpg'
+  },
+  {
+    key: 23,
+    img: 'gallery-23.jpg'
+  },
+  {
+    key: 24,
+    img: 'gallery-24.jpg'
+  },
+  {
+    key: 25,
+    img: 'gallery-25.jpg'
+  },
+  {
+    key: 26,
+    img: 'gallery-26.jpg'
+  },
+  {
+    key: 27,
+    img: 'gallery-27.jpg'
+  },
+  {
+    key: 28,
+    img: 'gallery-28.jpg'
+  },
+  {
+    key: 29,
+    img: 'gallery-29.jpg'
+  },
+  {
+    key: 30,
+    img: 'gallery-30.jpg'
+  },
+  {
+    key: 31,
+    img: 'gallery-31.jpg'
+  },
+  {
+    key: 32,
+    img: 'gallery-32.jpg'
+  },
+  {
+    key: 33,
+    img: 'gallery-33.jpg'
+  },
+  {
+    key: 34,
+    img: 'gallery-34.jpg'
+  },
+  {
+    key: 35,
+    img: 'gallery-35.jpg'
+  },
+  {
+    key: 36,
+    img: 'gallery-36.jpg'
+  },
+  {
+    key: 37,
+    img: 'gallery-37.jpg'
+  },
+  {
+    key: 38,
+    img: 'gallery-38.jpg'
+  },
+  {
+    key: 39,
+    img: 'gallery-39.jpg'
+  },
+  {
+    key: 40,
+    img: 'gallery-40.jpg'
+  },
+  {
+    key: 41,
+    img: 'gallery-41.jpg'
+  },
+  {
+    key: 42,
+    img: 'gallery-42.jpg'
+  },
+  {
+    key: 43,
+    img: 'gallery-43.jpg'
+  },
+  {
+    key: 44,
+    img: 'gallery-44.jpg'
+  },
+  {
+    key: 45,
+    img: 'gallery-45.jpg'
+  },
+  {
+    key: 46,
+    img: 'gallery-46.jpg'
+  },
+  {
+    key: 47,
+    img: 'gallery-47.jpg'
+  },
+  {
+    key: 48,
+    img: 'gallery-48.jpg'
+  },
+  {
+    key: 49,
+    img: 'gallery-49.jpg'
+  },
+  {
+    key: 50,
+    img: 'gallery-50.jpg'
+  },
+  {
+    key: 51,
+    img: 'gallery-51.jpg'
+  },
+  {
+    key: 52,
+    img: 'gallery-52.jpg'
+  },
+  {
+    key: 53,
+    img: 'gallery-53.jpg'
+  },
+  {
+    key: 54,
+    img: 'gallery-54.jpg'
+  },
+  {
+    key: 55,
+    img: 'gallery-55.jpg'
+  },
+  {
+    key: 56,
+    img: 'gallery-56.jpg'
+  },
+  {
+    key: 57,
+    img: 'gallery-57.jpg'
+  },
+  {
+    key: 58,
+    img: 'gallery-58.jpg'
+  },
+  {
+    key: 59,
+    img: 'gallery-59.jpg'
+  },
+  {
+    key: 60,
+    img: 'gallery-60.jpg'
+  },
+
+  {
+    key: 61,
+    img: 'gallery-61.jpg'
+  },
+  {
+    key: 62,
+    img: 'gallery-62.jpg'
+  },
+]
